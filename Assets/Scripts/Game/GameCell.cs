2adec581220c23a15489ff22bf45e1412f4c15f1
jbashiri <xumuk39rus@yandex.ru>
2019-06-19 23:21:07 +0300

    complete

---
+++ Assets/Scripts/Game/GameCell.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCell : MonoBehaviour
+{
+    public bool mine;
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject.transform.Find("green").gameObject);
+        print("!!!!!");
+    }
+}
---
+++ Assets/Scripts/Game/GameStart.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mime;
+using System.Xml.Linq;
+using UnityEngine;
+
+public class GameStart : MonoBehaviour
+{
+    public GameObject cell;
+    public Sprite mine;
+    public Sprite floor;
+    public Sprite flag;
+
+    public int mines = 5;
+
+    private GameObject[,] map = new GameObject[5,5];
+    // Start is called before the first frame update
+    void Start()
+    {
+        int x, y;
+        for (y = 0; y < 5; y++)
+        {
+            for (x = 0; x < 5; x++)
+            {
+                map[y, x] = Instantiate(cell);
+                map[y, x].transform.position = new Vector2(x * 1.5f, y * -1.5f);
+            }
+        }
+
+        while (mines >= 0)
+        {
+            x = Random.Range(0, 5);
+            y = Random.Range(0, 5);
+            if (map[y, x].GetComponent<GameCell>().mine == false)
+            {
+                map[y, x].GetComponent<SpriteRenderer>().sprite = mine;
+                map[y, x].GetComponent<GameCell>().mine = true;
+                map[y, x].transform.Find("number").gameObject.SetActive(false);
+                mines--;
+            }
+        }
+
+        for (y = 0; y < 5; y++)
+        {
+            for (x = 0; x < 5; x++)
+            {
+                map[y,x].transform.Find("number").GetComponent<TextMesh>().text = "" + checkMines(x, y);
+            }
+        }
+    }
+
+    int checkMines(int x, int y)
+    {
+        int xx, yy, mine;
+        mine = 0;
+        xx = -1;
+        yy = 1;
+        while (yy > -2)
+        {
+            xx = -1;
+            while (xx < 2)
+            {
+                if (y + yy >= 0 && y + yy < 5 && x + xx >= 0 && x + xx < 5 &&
+                    map[y + yy, x + xx].GetComponent<GameCell>().mine)
+                    mine++;
+                xx++;
+            }
+            yy--;
+        }
+        return (mine);
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
